d66022b1a5231cdadc11a7731616c8d38d89b53e
Dinesh763 <dksubudhy777@gmail.com>
2022-07-25 13:04:07 +0530

    77th Commit

---
+++ src/test/java/org/GitBash/Pratice/GitBashSample.java
@@ -0,0 +1,10 @@
+package org.GitBash.Pratice;
+
+public class GitBashSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("GitBash done again");
+	}
+
+}
