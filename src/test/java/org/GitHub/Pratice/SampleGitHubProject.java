c74eef8dc97d9472c2823cdab24d0970d0af3627
Dinesh763 <dksubudhy777@gmail.com>
2022-07-25 12:58:27 +0530

    7th commit

---
+++ src/test/java/org/GitHub/Pratice/SampleGitHubProject.java
@@ -0,0 +1,22 @@
+package org.GitHub.Pratice;
+
+import org.testng.annotations.Test;
+
+public class SampleGitHubProject {
+@Test
+public void GithubSample(){
+	System.out.println("DK");
+	//Again changes done
+	String BROWSER=System.getProperty("browser");
+	String URL=System.getProperty("url");
+	System.out.println(BROWSER+""+URL);
+}
+@Test
+public void GithubSample1(){
+	System.out.println("DKS");
+	//Changes Done    
+	
+	System.out.println("Done");
+}
+
+}
